e0dbe9b7e1ef92dd807ba9e417e382a65b16b1a4
chriskang <chriskang92@gmail.com>
2025-07-23 14:58:27 +0900

    proctiledeleted

--- restaurantproject/urls.py
+++ restaurantproject/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("restaurant.urls")),
+    path("", lambda request: HttpResponse("Hello from AIRestaurant!")),
 ]
